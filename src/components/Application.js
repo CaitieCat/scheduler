674cd5e75d5cebb8ce61584836f0ec528acc624d
Caitie Garrood <caitlinanneg@gmail.com>
2021-03-17 18:42:32 -0400

    Creates axios call for days and removes the hardcoding

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import DayList from "./DayList";
 import Appointment from "./Appointments"
 import "components/Application.scss";
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -68,6 +50,13 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    const requestURL = "http://localhost:8001/api/days"
+    axios.get(requestURL).then(response => 
+      setDays(response.data)
+      )
+}, [])
   return (
     <main className="layout">
       <section className="sidebar">
